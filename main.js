76061efd9b22e9c342457f623008dcfcf95a6bc9
Smitha <88016112+Smitha-4@users.noreply.github.com>
2022-01-24 04:46:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function add(){
+    var add=Number(document.getElementById("Number1").value) + Number(document.getElementById("Number1").value)
+document.getElementById("Answer").value=add
+}
+function sub(){
+    var sub=Number(document.getElementById("Number1").value) - Number(document.getElementById("Number1").value)
+document.getElementById("Answer").value=sub
+}
+function mul(){
+    var mul=Number(document.getElementById("Number1").value) * Number(document.getElementById("Number1").value)
+document.getElementById("Answer").value=mul
+}
+function div(){
+    var div=Number(document.getElementById("Number1").value) / Number(document.getElementById("Number1").value)
+document.getElementById("Answer").value=div
+}
